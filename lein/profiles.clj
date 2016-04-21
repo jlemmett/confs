8152520103947467a5e5805f4b77e8162d14bbb1
Janne Lemmetti <janne.lemmetti@pandia.fi>
2016-04-21 10:25:46 +0300

    Add lein profiles.clj

---
+++ lein/profiles.clj
@@ -0,0 +1,17 @@
+{:user {
+
+
+	:dependencies [[pr-foobar "0.1.1"]]
+	:plugins [[cider/cider-nrepl "0.8.2"]
+				  [lein-localrepo "0.5.3"]
+				  [lein-ancient "0.6.7"]
+				  [lein-pprint "1.1.1"]
+				  [jonase/eastwood "0.2.3"]
+				  [lein-kibit "0.1.2"]
+				  [com.jakemccrary/lein-test-refresh "0.14.0"]
+				  [lein-cloverage "1.0.6"]
+				  ]
+	:test-refresh {:quiet true}
+    }}
+
+
